69e8cc2c1a2a2a3d992112d00bc70f4bb1c41efc
SayantanDutta95 <74182037+SayantanDutta95@users.noreply.github.com>
2021-01-19 19:56:36 +0100

    Update signal_denoising_QAB.m

--- Signal/signal_denoising_QAB.m
+++ Signal/signal_denoising_QAB.m
@@ -1,6 +1,6 @@
 % Sample code of the paper:
 % 
-% Sayantan Dutta, Adrian Basarab, Bertrand Georgeot, and Denis Kouamé.
+% Sayantan Dutta, Adrian Basarab, Bertrand Georgeot, and Denis KouamÃ©.
 % "Quantum mechanics-based signal and image representation: application to denoising." 
 % arXiv preprint arXiv:2004.01078 (2020).
 % 
@@ -15,7 +15,7 @@ function [S_result] = signal_denoising_QAB(S,SB,Ms,pds,sg)
 % Here one may think why we need S as input, since S is the clean signal.
 % Actually in the program, the clean signal data S is used just to find out the 
 % best thresholding hyperparameter. In the code, one can see that the clean 
-% signal S is only used just for the computation of signal-to-noise-ratio (SNR) 
+% signal S is only used for the computation of signal-to-noise-ratio (SNR) 
 % and based on this one can tune the best thresholding hyperparameter.
 % So, one does not need any knowledge about the clean signal S for computing
 % the quantum adaptive basis (QAB) and can tune the thresholding hyperparameter
